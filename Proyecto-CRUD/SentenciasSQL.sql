a2fca07939e8394fa7d86c184595622ac011e34a
MigueGuille <93366590+MigueGuille@users.noreply.github.com>
2023-04-06 15:47:49 -0400

    Update SentenciasSQL.sql

--- Proyecto-CRUD/SentenciasSQL.sql
+++ Proyecto-CRUD/SentenciasSQL.sql
@@ -9,6 +9,10 @@
 
 -- INSERTAR
 INSERT INTO videojuego (Nombre, Genero, Año)  values ('Mario Kart 8 Deluxe', 'Carreras', '2017');
+INSERT INTO videojuego (Nombre, Genero, Año)  values ('Pac-Man', 'Arcade', '1990');
+INSERT INTO videojuego (Nombre, Genero, Año)  values ('The division', 'Disparos', '2016');
+INSERT INTO videojuego (Nombre, Genero, Año)  values ('CupHead', 'Run and Gun', '2017');
+INSERT INTO videojuego (Nombre, Genero, Año)  values ('The Last Of Us', 'Supervivencia', '2013');
 
 -- MOSTRAR 
 SELECT * FROM videojuego;
